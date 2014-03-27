8db8f729d55d91e4ea6042462c4cd9bbdd2e32f5
jose luis luna rubio <lunarubiojoseluis@gmail.com>
2014-03-27 13:35:18 -0600

    estructura base proyectos
    estructura base  a utilizar para los proyectos de chiwiscor

---
+++ index.php
@@ -0,0 +1,36 @@
+<!--by José Luis Luna Rubio (Chiwiscor)-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="author" content="José Luis Luna Rubio">
+    <title>-Chiwiscor-</title>
+    <link rel="shortcut icon" href="ima/favicon-chiwiscor.ico" />
+    <link type="text/plain" rel="author" href="humans.txt" />
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+    <script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+    <script type="text/javascript" src="js/funciones.js"></script>
+</head>
+<body>
+    <header>
+        <div id="raya"></div>
+        <div id="logo-chiwiscor"></div>
+        <div id="logo-programacion"></div>
+    </header>
+    <div id="container_principal"></div><!--/container_principal-->
+    <footer>by Jose Luis Luna Rubio (Chiwiscor)</footer>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49414817-1', 'joseluislunarubio.net');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+$(document).on("ready",chiwiscor);
+function chiwiscor()
+{
+	///$("html").niceScroll();
+	$("#container_principal").niceScroll({cursorcolor:"#9d490a"});
+}
